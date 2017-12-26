26d069b34b665164890295701d2cf9aaeee546d8
h908714124 <kraftdurchblumen@gmx.de>
2017-12-26 17:45:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<pre><code>$ mvn clean package
+$ java -jar target/cp.jar -r ~/Documents /media/archive 
+{"source":"/home/vgm/Documents", "dest":"/media/archive", "recursive":true}
+</code></pre>
---
+++ src/main/java/net/jbock/cp/CopyFile.java
@@ -0,0 +1,43 @@
+package net.jbock.cp;
+
+import java.util.Optional;
+import net.jbock.CommandLineArguments;
+import net.jbock.Description;
+import net.jbock.LongName;
+import net.jbock.Positional;
+import net.jbock.ShortName;
+
+public class CopyFile {
+
+  @CommandLineArguments(
+      programName = "cp",
+      missionStatement = "copy files and directories",
+      overview = "Copy SOURCE to DEST")
+  abstract static class Args {
+    @Positional abstract String source();
+    @Positional abstract String dest();
+
+    @ShortName('r')
+    @Description("copy directories recursively")
+    abstract boolean recursive();
+
+    @ShortName('b') @LongName("")
+    @Description("make a backup of each existing destination file")
+    abstract boolean backup();
+
+    @ShortName('S')
+    @Description("override the usual backup suffix")
+    abstract Optional<String> suffix();
+  }
+
+  public static void main(String[] args) {
+    CopyFile_Args_Parser.parse(args, System.out)
+        .ifPresentOrElse(
+            CopyFile::run,
+            () -> System.exit(1));
+  }
+
+  private static void run(Args args) {
+    System.out.println(args);
+  }
+}
