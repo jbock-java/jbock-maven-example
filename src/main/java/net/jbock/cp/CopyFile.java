517ab19eb969d386c770d9e6bed304bfa2605f3f
h908714124 <kraftdurchblumen@gmx.de>
2021-01-02 11:51:54 +0100

    update jbock

--- src/main/java/net/jbock/cp/CopyFile.java
+++ src/main/java/net/jbock/cp/CopyFile.java
@@ -3,7 +3,7 @@ package net.jbock.cp;
 public class CopyFile {
 
     public static void main(String[] input) {
-        Args args = Args_Parser.create().parseOrExit(input);
+        Args args = new Args_Parser().parseOrExit(input);
         System.out.println(args);
     }
 }
