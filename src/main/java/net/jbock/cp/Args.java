067e45bb406cdacf7e3a367fd3a1474ada927837
jbock <jbock-java@gmx.de>
2023-11-16 19:22:39 +0100

    add a test

--- src/main/java/net/jbock/cp/Args.java
+++ src/main/java/net/jbock/cp/Args.java
@@ -4,47 +4,44 @@ import net.jbock.Command;
 import net.jbock.Option;
 import net.jbock.Parameter;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.nio.file.Path;
 import java.util.Optional;
-import java.util.StringJoiner;
 
 /**
  * Copy SOURCE to DEST
  */
 @Command(name = "cp")
-abstract class Args {
+interface Args {
 
     /**
      * Path or file of directory to copy.
      * @return SOURCE
      */
     @Parameter(index = 0)
-    abstract Path source();
+    Path source();
 
     /**
      * Copy destination
      * @return DEST
      */
     @Parameter(index = 1)
-    abstract Path dest();
+    Path dest();
 
     /**
      * Copy directories recursively
      */
     @Option(names = {"--recursive", "-r"})
-    abstract boolean recursive();
+    boolean recursive();
 
     /**
      * Make a backup of each existing destination file
      */
     @Option(names = {"--backup", "-b"})
-    abstract boolean backup();
+    boolean backup();
 
     /**
      * Override the usual backup suffix
      */
     @Option(names = {"--suffix", "-s"})
-    abstract Optional<String> suffix();
+    Optional<String> suffix();
 }
---
+++ src/test/java/net/jbock/cp/ArgsTest.java
@@ -0,0 +1,16 @@
+package net.jbock.cp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArgsTest {
+
+    @Test
+    void testPrint() {
+        Args result = new ArgsParser().parse(List.of("1", "2", "-rbs12")).getRight().orElseThrow();
+        assertEquals("{recursive: true, backup: true, suffix: Optional[12], source: 1, dest: 2}", result.toString());
+    }
+}
